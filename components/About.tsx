3abb7950a997ef9796e93228fceaef15eaaf0a61
Mike O'Bank <mike.obank@pm.me>
2025-10-08 16:54:55 +0200

    About text

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,7 @@ const About = () => {
     <div class="py-4">
       <h3>About</h3>
       <p>
-        A way to encode vanity names within public keys. Plus an additional fingerprint encoded with emoji characters. Allowing for unique names, where duplicates (conflicting name + fingerprint pairs) will be exponentialy hard to find (brute force).
+        A protocol to encode vanity names within public keys. Plus an additional fingerprint encoded using emoji characters. Allowing for unique names, where duplicates (conflicting pairs of name & fingerprint) will be exponentialy hard to brute force.
       </p>
       <p class="href-wrap">
         <a href="https://github.com/mikeobank/vanice/blob/main/vanice.md">https://github.com/mikeobank/vanice/blob/main/vanice.md</a>
