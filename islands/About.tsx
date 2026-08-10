309ed4aa0bf242109d3e4f617ea22fe1d4a296b2
Mike O'Bank <mike.obank@pm.me>
2026-08-10 15:14:17 +0200

    About

--- islands/About.tsx
+++ islands/About.tsx
@@ -5,7 +5,7 @@ const About: FunctionComponent = () => {
     <div class="py-4">
       <h3>About</h3>
       <p>
-        A protocol to encode vanity names within public keys. Plus an additional fingerprint encoded using emoji characters. Allowing for unique names, where duplicates (conflicting pairs of name & fingerprint) will be exponentially hard to brute force.
+        A protocol to encode vanity names within public keys. Using emoji characters to create a fingerprint. Allowing for unique names, where duplicates (conflicting pairs of name & fingerprint) will be exponentially hard to brute force. In addition a CRDT is available to create and sign a distributed dataset for defining a Web of Trust and general data graphs.
       </p>
       <p class="href-wrap">
         <a href="https://github.com/onamea/docs/blob/main/onamea.md">https://github.com/onamea/docs/blob/main/onamea.md</a>
