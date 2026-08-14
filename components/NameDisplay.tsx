32b16b390ec49a5e58eb625cf6accdb39600e4f2
Mike O'Bank <mike.obank@pm.me>
2026-08-14 15:52:54 +0200

    NameDisplay tooltip

--- components/NameDisplay.tsx
+++ components/NameDisplay.tsx
@@ -2,7 +2,7 @@ import { useSignal } from "@preact/signals"
 import { type FunctionComponent } from "preact"
 import { useEffect } from "preact/hooks"
 import type { PrimaryKey, Name } from "@onamea/types"
-import { primaryKeyToFingerprintedName, toNameKey } from "@onamea/types"
+import { displayFingerprint, primaryKeyToFingerprint, primaryKeyToFingerprintedName, toNameKey } from "@onamea/types"
 
 type Props = {
   primaryKey: PrimaryKey
@@ -13,25 +13,30 @@ type Props = {
 const NameDisplay: FunctionComponent<Props> = ({ primaryKey, name, shouldLink = false }) => {
 
   const nameKey = useSignal<string>()
-  const fingerprintedName = useSignal<string>()
   const fingerprint = useSignal<string>()
+  const fullFingerprint = useSignal<string>()
 
   useEffect(() => {
     nameKey.value = toNameKey(name, primaryKey)
     ;(async () => {
-      const [_fingerprintedName,, _fingerprint] = await primaryKeyToFingerprintedName(primaryKey, name)
-      fingerprintedName.value = _fingerprintedName
+      const [,, _fingerprint] = await primaryKeyToFingerprintedName(primaryKey, name)
       fingerprint.value = _fingerprint
+      fullFingerprint.value = displayFingerprint(await primaryKeyToFingerprint(primaryKey))
     })()
   }, [primaryKey, name])
 
+  const title = fullFingerprint.value !== undefined ? `${ name }${ fullFingerprint.value }` : name
+  const shouldRender = nameKey.value !== undefined && fingerprint.value !== undefined
+
   return (
-    <span class="name-display">
-      { shouldLink ?
-        <a href={`/identity/${ nameKey.value }`}>{ name }<span class="fingerprint">{ fingerprint.value ?? "" }</span></a> :
-        <>{ name }<span>{ fingerprint.value ?? "" }</span></>
+    shouldRender
+    ? <span class="name-display">
+      { shouldLink
+        ? <a href={`/identity/${ nameKey.value }`} title={ title }>{ name }<span class="fingerprint">{ fingerprint.value ?? "" }</span></a>
+        : <span title={ title }>{ name }<span>{ fingerprint.value ?? "" }</span></span>
       }
-    </span> 
+      </span> 
+    : <span class="name-display">{ name }</span>
   )
 }
 
