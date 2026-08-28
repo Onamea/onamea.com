a3a56580c237c6149c35e100dd8de8ffad928959
Mike O'Bank <mike.obank@pm.me>
2026-08-28 22:18:56 +0200

    Display body as multiline

--- components/IdentityDisplay.tsx
+++ components/IdentityDisplay.tsx
@@ -33,7 +33,7 @@ const IdentityDisplay: FunctionComponent<Props> = ({ identity }) => {
       <dl>
         <div><dd>id</dd><dt>{ identity.id }</dt></div>
         <div><dd>fingerprint</dd><dt>{ fingerprintDisplay.value ?? "" }</dt></div>
-        <div><dd>body</dd><dt>{ identity.body ?? "-" }</dt></div>
+        <div><dd>body</dd><dt className="multiline">{ identity.body ?? "-" }</dt></div>
         <div><dd>tombstone</dd><dt>{ String(identity.tombstone) }</dt></div>
         <div>
           <dd>sub keys</dd>
